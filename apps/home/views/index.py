b02c66209ddcdc79f2056a37e5046ce62cfc8753
Claude Tan23 <tanyongsen99@hotmail.com>
2026-03-11 10:38:04 +0800

    Add config(Dev): Add docker configuration for development environment

--- apps/home/views/index.py
+++ apps/home/views/index.py
@@ -1,18 +1,29 @@
+from django.utils import timezone
 from django import template
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.shortcuts import render
-from apps.home.models import Product
+from apps.home.models import Product, Sale
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST, require_GET
 import logging
+from django.db.models import Sum
 
 def dashboard(request):
     products = Product.objects.all()
+
+    today = timezone.now().date()
+
+    today_total_sales = Sale.objects.filter(
+        sale_date__date=today
+    ).aggregate(total=Sum('total_amount'))['total'] or 0
+
+    total_stock = products.aggregate(total=Sum('stock'))
+    out_of_stock = products.filter(stock=0).count()
 
     labels = [p.name for p in products]
     quantities = [p.stock for p in products]
@@ -20,6 +31,9 @@ def dashboard(request):
     context = {
         'labels': labels,
         'quantities': quantities,
+        'totalStock': total_stock,
+        'total_outStock': out_of_stock,
+        'totalSale': today_total_sales
     }
     
     logger = logging.getLogger("core")
